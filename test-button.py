71db48ec9d5b24e645705639f3ce3ace7c23f048
Joshua Schmidt <jschmidtnj@gmail.com>
2018-04-17 18:11:25 -0400

    Update test-button.py

--- test-button.py
+++ test-button.py
@@ -6,7 +6,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-    input_state = GPIO.input(18)
+    input_state = GPIO.input(3)
     if input_state == False:
         print('Button Pressed')
         time.sleep(0.2)
