4aedb7bd9fa2675f3d48f31b053c188361e5e959
jschmidtnj <jschmidtnj@gmail.com>
2018-04-22 13:01:50 -0400

    small changes

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -212,7 +212,7 @@ window.onload = function() {
 $(document).ready(function(){
 	$(window).on("scroll",function(){
   	var wn = $(window).scrollTop();
-    if(wn > 200){
+    if(wn > 800){
     	$(".navbar").css("background","rgba(255,255,255,.5)");
     }
     else{
@@ -220,19 +220,3 @@ $(document).ready(function(){
     }
   });
 });
-
-$(document).ready(function(){       
-			var scroll_start = 0;
-			var startchange = $('#startchange');
-			var offset = startchange.offset();
-			if (startchange.length){
-				$(document).scroll(function() { 
-					scroll_start = $(this).scrollTop();
-					if(scroll_start > offset.top) {
-						$(".navbar-default").css('background-color', '#f0f0f0');
-					} else {
-						$('.navbar-default').css('background-color', 'transparent');
-					}
-				});
-			}
-		});
