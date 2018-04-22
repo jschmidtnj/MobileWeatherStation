5a23b0738b9e33f81387ec08ccb06bc416be9c7a
jschmidtnj <jschmidtnj@gmail.com>
2018-04-22 15:53:46 -0400

    added google sheets integration

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -220,3 +220,18 @@ $(document).ready(function(){
     }
   });
 });
+
+var $form = $('form#test-form'),
+    url = 'https://script.google.com/macros/s/AKfycbz77HuNxSiVaaODk46yAl8XZRTLS_kMGUUY5-zAEqJg9eplBrQ/exec'
+
+$('#submit-form').on('click', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serializeObject()
+  }).success(
+    // do something
+  );
+})
