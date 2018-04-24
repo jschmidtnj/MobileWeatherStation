2c921ce67f38cf6c9d88c4b1106c62fa2b60425a
jschmidtnj <jschmidtnj@gmail.com>
2018-04-23 22:03:11 -0500

    added some shell fixes

--- startup.sh
+++ startup.sh
@@ -1,3 +1,8 @@
 cd /home/pi/Desktop/MobileWeatherStation
 git pull origin master
+ip4=$(/sbin/ip -o -4 addr list wlan0 | awk '{print $4}' | cut -d/ -f1)
+echo $ip4 >> ip-address.txt
+git add -A
+git commit -m "added ip-addresses"
+git push origin master
 python3 app.py
