aad09d7e3ad5f9be87e26e2e5f4d4b16948d3e61
Joshua Schmidt <jschmidtnj@gmail.com>
2018-04-29 18:02:34 -0400

    Update markdown-script.js

--- docs/js/markdown-script.js
+++ docs/js/markdown-script.js
@@ -1,6 +1,6 @@
 window.onload = function(){
 jQuery.get("https://rawgit.com/jschmidtnj/MobileWeatherStation/master/FinalReport/README.md", function(data) {
-    var text = document.getElementById('sourceTA').value,
+    var text = data,
         target = document.getElementById('markdown'),
         converter = new showdown.Converter(),
         html = converter.makeHtml(text);
